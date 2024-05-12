1d715f5e5e65f543f7f1907043e0064705d79b20
ucanfindme18 <39616391+ucanfindme18@users.noreply.github.com>
2024-05-13 01:20:53 +0800

    Update kapamilya.py

--- scripts/kapamilya.py
+++ scripts/kapamilya.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+banner = r''
 import requests
 import os
 import sys
@@ -9,18 +10,18 @@ if 'win' in sys.platform:
     windows = True
 
 def grab(url):
-    response = s.get(url, timeout=15).text
+    response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
-        response = requests.get(url).text
+        #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/ucanfindme18/GMA7/main/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/ucanfindme18/GMA7/main/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
@@ -32,16 +33,13 @@ def grab(url):
             break
         else:
             tuner += 5
-    streams = s.get(link[start:end]).text.split('#EXT')
-    hd = streams[-1].strip()
-    st = hd.find('http')
-    print(hd[st:].strip())
-    #print(f"{link[start : end]}")
+    print(f"{link[start : end]}")
 
 print('#EXTM3U')
-print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
-s = requests.Session()
+print('#EXT-X-INDEPENDENT-SEGMENTS')
+
+print(banner)
+#s = requests.Session()
 with open('../kapamilya.txt') as f:
     for line in f:
         line = line.strip()
@@ -53,9 +51,10 @@ with open('../kapamilya.txt') as f:
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
+            print(f'')
         else:
             grab(line)
-
+            
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
     os.system('rm watch*')
